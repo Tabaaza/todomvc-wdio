19b83e09c6866371f4541f1188c906f555112938
Omar Tabaza <omar.tabaza@gmail.com>
2022-09-03 09:54:19 +0200

    README and Add one more testcases (I dreamed about 🐶)

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # todomvc-wdio-FV
 E2E automated test for todomvc using #WDIO
+
+FirstVet Automation Challenge
+
+## This project is built with:!
+- WebdriverIO
+- Mocha
+
+## Installation
+
+### WebdriverIO
+All the requierd software and packages you will find here: [WebdriverIO](https://webdriver.io/docs/gettingstarted.html)
+
+## How to run the Test:
+- Open your Terminal
+- Create dir ($ mkdir) where you want to clone the project 
+- clone the project [https://github.com/Tabaaza/todomvc-wdio-FV.git](https://github.com/Tabaaza/todomvc-wdio-FV.git)
+- Wait until complite and the project ready
+- Open Treminal with the project path and type:
+```sh
+npm run test 
+```
+
+--- 
+
+### Testcases
+
+1. As User, Should Home page load as expected.
+2. As User, Should be able to add New Todo
+3. As User, Should delete todo
+4. As User, Should mark todo as Complete
+5. As User, Should find todo in Copmlete tab
+6. As User, Should not find the completed todo in Active tab
+7. As User, Should Mark all as complete
+8. As User, Should Clear all Completed Items and back empty stat
+
+
+---
+### Authors
+Omar Tabaza 
--- test/specs/todo.e2e.js
+++ test/specs/todo.e2e.js
@@ -1,13 +1,15 @@
 const todo = require('../pageobjects/todo.page');
 
 describe('Create My Todo list', () => {
-    it('should Home page load as expected', async () => {
+    it('Should Home page load as expected', async () => {
         await todo.open();
         expect(todo.headerH1).toBeDisplayed();
     });
 
     it('Should be able to add New Todo', async () => {
-        await todo.addNewTodoAction('Milk ');
+        await todo.addNewTodoAction('Milk');
+        await todo.changeItemText('Milk')
+        expect(todo.todoItemByText).toHaveText('Milk') // Verify new todo added correctly. 
         expect(todo.itemsCount).toHaveText('1');
         await todo.addNewTodoAction('Apple');
         await todo.addNewTodoAction('Tony\'s Chocolonely');
@@ -33,6 +35,16 @@ describe('Create My Todo list', () => {
         expect(todo.firstTaskStatic).toExist()
     });
     
+    it('Should not find the completed todo in Active tab', async () =>{
+        await todo.completedTab.click();
+        elmCom = await todo.firstTask.getText();
+        await todo.activeTab.click();
+        await todo.changeItemText(elmCom);
+        elmAct = await todo.todoItemByText.isExisting();
+        expect(!elmAct);
+
+    });
+   
     it('Should Mark all as complete', async () =>{
         await todo.activeTab.click()
         await todo.markAllAsComplete.click()
